d3283f2ef710d0c97b2049bd6c40b7d659a54696
Sylvain Premel <spremel@anevia.com>
2020-06-07 18:33:28 +0200

    Update server root

--- install.sh
+++ install.sh
@@ -2,14 +2,13 @@
 set -e
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TARGET=/var/www
+TARGET=/var/www/html
 
 cd $THIS_DIR/client
 rm -f dist/*
 
 npm install
 npm run-script build
-mv $TARGET/html $TARGET/html.old
-cp -r dist $TARGET/html
-
-cp -r $THIS_DIR/server/data /var/www
+mv $TARGET $TARGET.old
+cp -r dist $TARGET
+cp -r $THIS_DIR/server/data $TARGET
