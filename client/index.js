948890d6521cda757f9aa998851156c6cb87b397
Sylvain Premel <spremel@anevia.com>
2020-06-07 18:11:27 +0200

    Cleaning

--- client/index.js
+++ client/index.js
@@ -9,28 +9,24 @@ import GPX from 'ol/format/GPX';
 var blur = document.getElementById('blur');
 var radius = document.getElementById('radius');
 
-var source = new VectorSource({
-  url: 'http://localhost/data/test/constance.gpx',
+var serverIp = "35.210.237.237"
+var vectorSource = new VectorSource({
+  url: 'http://' + serverIp + '/data/test/constance.gpx',
   format: new GPX()
 });
 
-
-var heatmapLayer = new HeatmapLayer({
-  source: source,
+var heatmap = new HeatmapLayer({
+  source: vectorSource,
   blur: parseInt(blur.value, 10),
-  radius: parseInt(radius.value, 10),
-  // weight: function(feature) {
-  //   return 10;
-  // }
+  radius: parseInt(radius.value, 10)
 });
 
-var tileSource = new ol.source.OSM()
 var raster = new TileLayer({
-  source: tileSource
+  source: new ol.source.OSM()
 });
 
 new Map({
-  layers: [raster, heatmapLayer],
+  layers: [raster, heatmap],
   target: 'map',
   view: new View({
     center: [0, 0],
