72de1673181cc174a8c9b4d55a332c00721ef1b2
Sylvain Premel <spremel@anevia.com>
2020-06-21 19:54:36 +0200

    Update deployment script

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+if [[ $EUID -ne 0 ]]; then
+   1>&2 echo "This script must be run as root" 
+   exit 1
+fi
+
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TARGET=/var/www/html
 
 cd $THIS_DIR/client
-rm -f dist/*
+rm -rf dist/*
 # npm install
 npm run build
 rm -rf $TARGET.old
